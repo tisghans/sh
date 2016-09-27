a16efbdb48cb28f02c39b510378d346765c2fb04
Gurinder Hans <hello@gurinderhans.me>
2016-09-27 03:40:25 -0700

    install Vundle plugins using install.sh, remove master.tar.gz

--- install/install.sh
+++ install/install.sh
@@ -3,12 +3,15 @@ WD=$HOME/$INSTALLATION_FOLDER
 rm -rf $WD
 mkdir -p $WD;
 cd $WD; curl -OL https://github.com/gurinderhans/.cbash/archive/master.tar.gz;
-	tar -xzvf master.tar.gz;
+	tar -xzvf master.tar.gz; rm -rf master.tar.gz
 
 # install Vundle
 cd $WD/.cbash-master/vim; mkdir -p bundle; cd bundle;
 	curl -OL https://github.com/VundleVim/Vundle.vim/archive/master.tar.gz;
 	tar -xzvf master.tar.gz; mv Vundle.vim-master Vundle.vim; rm master.tar.gz;
+
+# install vundle plugins
+vim +PluginInstall +qall
 
 # backup prev config
 if [ -f $HOME/.bash_profile ]; then
