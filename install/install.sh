69d2ac19a5dcfa0f22fe0aabde56b57dfa0e794f
Gurinder Hans <hello@gurinderhans.me>
2016-09-27 02:49:43 -0700

    be compatible with older git versions for macOS

--- install/install.sh
+++ install/install.sh
@@ -1,3 +1,3 @@
-git clone --recursive -j8 git@github.com:gurinderhans/.cbash.git $HOME/.cbash
+git clone --recursive git@github.com:gurinderhans/.cbash.git $HOME/.cbash
 mv $HOME/.bash_profile $HOME/.bash_profile.bk
 echo "source ~/.cbash/profiles/bash_profile" >> $HOME/.bash_profile
