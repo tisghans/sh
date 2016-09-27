dbfb69901a4d30cd35ddb2fc185b9572c2f5b704
Gurinder Hans <hello@gurinderhans.me>
2016-09-27 02:35:09 -0700

    fix install script

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,6 @@
 cd $HOME; curl -OL https://github.com/gurinderhans/.cbash/archive/master.tar.gz 
 tar -xzvf $HOME/master.tar.gz
+cd $HOME; mv .cbash-master .cbash
 rm $HOME/master.tar.gz
 mv $HOME/.bash_profile $HOME/.bash_profile.bk
-echo "source ~/.cbash-master/profiles/bash_profile" >> $HOME/.bash_profile
+echo "source ~/.cbash/profiles/bash_profile" >> $HOME/.bash_profile
