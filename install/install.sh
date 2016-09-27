258c764fedf7ae37ba03ff23ccc4e4987dfb187f
Gurinder Hans <hello@gurinderhans.me>
2016-09-27 03:28:28 -0700

    update install and uninstall scripts, update bash_profile and vimrc to support that

--- install/install.sh
+++ install/install.sh
@@ -1,9 +1,20 @@
-WD=$HOME/.cbash-gurinderhans
+INSTALLATION_FOLDER=.cbash-gurinderhans
+WD=$HOME/$INSTALLATION_FOLDER
 rm -rf $WD
 mkdir -p $WD;
 cd $WD; curl -OL https://github.com/gurinderhans/.cbash/archive/master.tar.gz;
-	tar -xzvf master.tar.gz
+	tar -xzvf master.tar.gz;
 
+# install Vundle
+cd $WD/.cbash-master/vim; mkdir -p bundle; cd bundle;
+	curl -OL https://github.com/VundleVim/Vundle.vim/archive/master.tar.gz;
+	tar -xzvf master.tar.gz; mv Vundle.vim-master Vundle.vim; rm master.tar.gz;
 
+# backup prev config
+if [ -f $HOME/.bash_profile ]; then
+	cd $HOME; mv .bash_profile .bash_profile.bk
+fi
 
-#echo "source ~/.cbash/profiles/bash_profile" >> $HOME/.bash_profile
+# create new config
+echo "export CBASH_DIR=$WD/.cbash-master/;\
+	source $WD/.cbash-master/profiles/bash_profile" > $HOME/.bash_profile
--- install/uninstall.sh
+++ install/uninstall.sh
@@ -1,4 +1,12 @@
-rm -rf $HOME/.cbash
-rm -rf $HOME/.bash_profile
-mv $HOME/.bash_profile.bk $HOME/.bash_profile
+INSTALLATION_FOLDER=.cbash-gurinderhans
+WD=$HOME/$INSTALLATION_FOLDER
+rm -rf $WD
+
+if [ -f $HOME/.bash_profile ]; then
+	rm -rf $HOME/.bash_profile
+fi
+
+if [ -f $HOME/.bash_profile.bk ]; then
+	mv $HOME/.bash_profile.bk $HOME/.bash_profile
+fi
 
