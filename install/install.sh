d99fae05d1f958cb208b1ee8e4d9880e8a6db332
Gurinder Hans <hello@gurinderhans.me>
2016-10-01 20:02:21 -0700

    update install script, include .bashrc support

--- install/install.sh
+++ install/install.sh
@@ -11,11 +11,14 @@ cd $WD/.cbash/vim; mkdir -p bundle; cd bundle;
 	curl -OL https://github.com/VundleVim/Vundle.vim/archive/master.tar.gz;
 	tar -xzvf master.tar.gz; mv Vundle.vim-master Vundle.vim; rm master.tar.gz;
 
-# backup prev config
+
+# backup previous config and replace with newer one
+new_config="export CBASH_DIR=$WD/.cbash/;\ source $WD/.cbash/profiles/bash_profile" 
+
 if [ -f $HOME/.bash_profile ]; then
 	cd $HOME; mv .bash_profile .bash_profile.bk
+	echo $new_config > $HOME/.bash_profile
+elif [ -f $HOME/.bashrc ]; then
+	cd $HOME; mv .bashrc .bashrc.bk
+	echo $new_config > $HOME/.bashrc
 fi
-
-# create new config
-echo "export CBASH_DIR=$WD/.cbash/;\
-	source $WD/.cbash/profiles/bash_profile" > $HOME/.bash_profile
--- install/uninstall.sh
+++ install/uninstall.sh
@@ -4,9 +4,12 @@ rm -rf $WD
 
 if [ -f $HOME/.bash_profile ]; then
 	rm -rf $HOME/.bash_profile
+else if [ -f $HOME/.bashrc ]; then
+	rm -rf $HOME/.bashrc
 fi
 
 if [ -f $HOME/.bash_profile.bk ]; then
 	mv $HOME/.bash_profile.bk $HOME/.bash_profile
+else if [ -f $HOME/.bashrc.bk ]; then
+	mv $HOME/.bashrc.bk $HOME/.bashrc
 fi
-
